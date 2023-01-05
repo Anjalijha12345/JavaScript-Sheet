dcf95692f1a0b8058f750115e4e3a8bae5977241
Anjalijha12345 <anjalijha042@gmail.com>
2023-01-05 20:14:55 +0530

    JavaScript Structure

---
+++ Day1.js/answer5.js
@@ -0,0 +1,8 @@
+var str="Anjali";
+var bool=true;
+var unknown=undefined;
+var empty=null;
+console.log(typeof str);
+console.log(typeof condition);
+console.log(typeof unknown);
+console.log(typeof empty);
