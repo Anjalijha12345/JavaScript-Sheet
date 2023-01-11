56d782192124379efaf987978943d9b0ad06d4ab
Anjalijha12345 <anjalijha042@gmail.com>
2023-01-11 21:13:14 +0530

    JavaScript Structure

---
+++ Day3/Easy/answer6.js
@@ -0,0 +1,24 @@
+// 4>3 && 10<12     true
+// 4>3 && 10>12     false
+// 4>3 || 10<12     true
+//  4>3 || 10>12     true
+//  !(4>3)           false
+//  !(4<3)           true
+//  !(false)         true
+//  !(4>3 && 10<12)  false
+//  !(4>3 && 10>12)  true
+//   !(4=='4')       true
+
+
+console.log("true");
+console.log("false");
+console.log("true");
+console.log("truee");
+console.log("false");
+console.log("true");
+console.log("true");
+console.log("false");
+console.log("false");
+console.log("true");
+console.log("true");
+console.log("pyth".length>"Drag".length);
