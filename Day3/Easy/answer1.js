9a359a2bb5a9f94dd628e9203dbc51ec90538d4b
Anjalijha12345 <anjalijha042@gmail.com>
2023-01-11 20:27:25 +0530

    JavaScript structure

---
+++ Day3/Easy/answer1.js
@@ -0,0 +1,17 @@
+let firstName="Anjali";
+let lastName="jha";
+let country="India";
+let city="Durgapur"
+let age="21";
+let isMarried=false;
+let year="2023";
+
+console.log(typeof(firstName))
+console.log(typeof(lastName))
+console.log(typeof(country))
+console.log(typeof(age))
+console.log(typeof(isMarried))
+console.log(typeof(year))
+
+
+
