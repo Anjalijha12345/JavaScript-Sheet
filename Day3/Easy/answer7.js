7a96fee41f73dcff241588b28c677071c9e85a9a
Anjalijha12345 <anjalijha042@gmail.com>
2023-01-11 21:24:07 +0530

    JavaScript Structure

---
+++ Day3/Easy/answer7.js
@@ -0,0 +1,9 @@
+const date= new Date();
+console.log("Year :",date.getFullYear());
+console.log("Month: ",date.getMonth()+1);
+console.log("Date:",date.getDate());
+console.log("Day :",date.getDay());
+console.log("Hours :",date.getHours());
+console.log("Mins:",date.getMinutes());
+console.log("secs :",Date.now());
+
