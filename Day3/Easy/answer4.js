5258a44d1cd23f836ce1cf98c004b780d26109c0
Anjalijha12345 <anjalijha042@gmail.com>
2023-01-11 20:59:14 +0530

    JavaScript Structure

---
+++ Day3/Easy/answer4.js
@@ -0,0 +1,9 @@
+//1. truthy value
+console.log(1<2)
+console.log("a".length>"".length);
+console.log(typeof true==typeof false);
+
+//2.falsy  value
+console.log(1>2)
+console.log("a".length<"".length)
+console.log(typeof true!==typeof false);
---
+++ Day3/Easy/answer5.js
@@ -0,0 +1,25 @@
+1.// 4>3    True
+2.// 4>=3   True
+3.// 4 < 3   false
+4.// 4 <=3   false
+5.// 4==4    True
+6.// 4==4    True
+7.// 4!= 4   false
+8. // 4!== 4  false
+9.//  4!='4'  false
+10.//4 == '4'  True
+11.//4==4     false
+
+console.log("True");
+console.log("True");
+console.log("false");
+console.log("false");
+console.log("True");
+console.log("True");
+console.log("false");
+console.log("false");
+console.log("false");
+console.log("True");
+console.log("false");
+
+console.log("python.length > Jagron.length");
