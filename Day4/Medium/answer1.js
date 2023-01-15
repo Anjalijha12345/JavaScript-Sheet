1cfdacfeed7ec537ed38e99e2e168f8059720c4e
Anjalijha12345 <anjalijha042@gmail.com>
2023-01-15 10:49:28 +0530

    Javascript structure

---
+++ Day4/Medium/answer1.js
@@ -0,0 +1,21 @@
+let score=parseInt(prompt("Enter your score:"));
+if(score>=80 && score<=100){
+    console.log("A");
+}
+    else if(score>=70 && score<=89){
+        console.log("B");
+    }
+else if(SCORE>=60 && score<=69)
+{
+    console.log("C");
+}
+
+else if( SCORE>=50 && score<=59)
+{
+    console.log("D");
+
+}
+else
+{
+    console.log("F");
+}
